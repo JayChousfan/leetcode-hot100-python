48898c581c141c1c8853126201bfc575bf109d6d
JayChousfan <JayChousfan@users.noreply.github.com>
2026-09-07 22:06:22 +0800

    Complete Two Sum practice solution

--- easy/practice/e01_two_sum.py
+++ easy/practice/e01_two_sum.py
@@ -2,6 +2,13 @@
 函数：fun(nums, target) -> list[int]"""
 # TODO: 在这里写出完整的 def fun(...): 函数
 def fun(nums: list[int],target:int) -> list[int]:
+    mp:dict[int,int] ={}
+    for i,num  in enumerate(nums):
+        complement =target - num
+        if complement in mp:
+            return [mp[complement],i]
+        mp[num] = i
+    return []
 
 if __name__ == "__main__":
     assert fun([2, 7, 11, 15], 9) == [0, 1], "示例1失败"
